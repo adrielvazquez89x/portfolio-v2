ec8cfc91716680d13127cf29c5898464d6fc4e64
Adriel Vazquez <adrialvaz@hotmail.com>
2024-02-27 14:55:20 -0300

    db conection

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,28 @@
 import styles from "./projects.module.css";
 import Card from "@/components/card/card";
-import projects from "../../data/projects";
 import Project from "../types/project";
+import { sql } from "@vercel/postgres";
 
-export default function Projects() {
 
-    const sites: Project[] = projects;
+export default async function Projects() {
+
+    const { rows } = await sql`SELECT * FROM projects`;
+
+    const projects: Project[] = [];
+
+    for (const item of rows) {
+        let aux: Project = {
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            git: item.giturl,
+            deploy: item.deployurl,
+            img: item.imgurl,
+            tech: item.tech
+        }
+        projects.push(aux);
+        
+    }
 
     return (
 
@@ -13,7 +30,7 @@ export default function Projects() {
             <h1 className={styles.title}>My projects</h1>
             <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center px-5">
                 {
-                    sites.map((site) => (
+                    projects.map((site) => (
                         <Card project={site} key={site.id} />
 
                     ))
